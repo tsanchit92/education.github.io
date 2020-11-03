974fbca6547258f6b9925a3eecf0e63a41921345
tsanchit92 <68181161+tsanchit92@users.noreply.github.com>
2020-11-03 22:05:13 +0530

    Add files via upload

---
+++ SITE/javascript/login.js
@@ -0,0 +1,26 @@
+var attempt = 3; 
+
+
+function validate(){
+	var username = document.getElementById("username").value;
+	var password = document.getElementById("password").value;
+
+	if ( username == "E-educate" && password == "e123456789")
+	{
+		alert ("Login successfully");
+		window.location = "success.html"; 
+		return false;
+	}
+	else{
+		attempt --;
+		alert("Incorrect Password !! You have left "+attempt+" attempt;");
+		
+		
+		if( attempt == 0){
+			document.getElementById("username").disabled = true;
+			document.getElementById("password").disabled = true;
+			document.getElementById("submit").disabled = true;
+			return false;
+		}
+	}
+}
